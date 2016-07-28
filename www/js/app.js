1267569557a58d1766414e1f8394c72db2f1e809
yuyang545262477 <yuyang545262477@gmail.com>
2016-07-28 11:21:42 +0800

    add sidemenu

--- www/js/app.js
+++ www/js/app.js
@@ -4,24 +4,30 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
-    .config(function ($statProvider, $urlRouterProvider) {
-        $statProvider
-            .state('app',{
-                url:'/app',
-                templateUrl:'templates/menu.html',
-                controller:'AppCtrl',
-                abstract:true
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('app', {
+                url: '/app',
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl',
+                abstract: true
             })
-            .state('app.home',{
-                url:'/home',
-                views:{
-                    'menuContent':{
-                        templateUrl:'templates/home.html',
-                        controller:'HomeCtrl'
+            .state('app.home', {
+                url: '/home',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/home.html',
+                        controller: 'HomeCtrl'
                     }
                 }
             });
         $urlRouterProvider.otherwise('app/home');
+    })
+    .controller('AppCtrl', function () {
+
+    })
+    .controller('HomeCtrl', function () {
+
     })
 
     .run(function ($ionicPlatform) {
